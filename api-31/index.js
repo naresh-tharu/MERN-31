ccf870b117da3e360ab918ac5daf470ee317cd66
Naresh Tharu <jr.er.nareshtharu@gmail.com>
2024-07-18 20:32:24 +0545

    server created and express app mount

---
+++ api-31/index.js
@@ -0,0 +1,12 @@
+import http, { createServer } from "http";
+import app from "./src/config/express.config.js";
+import "dotenv/config";
+const server = createServer(app);
+
+const port = process.env.PORT || 3005;
+server.listen(port, "127.0.0.1", (err) => {
+  if (!err) {
+    console.log(`Server is running on the port:${port}`);
+    console.log(`Press ctrl+c to discontinue the server`);
+  }
+});
